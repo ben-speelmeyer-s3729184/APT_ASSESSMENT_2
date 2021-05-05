a23e0d7c5b0e418493616a3db2ea82f57c28bb48
Matt <matt@curiousit.com>
2021-05-06 09:20:56 +1000

    Player
    minor fixes - add return value to getPlayerScore

--- GameState.h
+++ GameState.h
@@ -1,25 +1,23 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
 
-#include "LinkedList.h"
 #include <iostream>
 
-class GameState
-{
-private:
+#include "LinkedList.h"
+
+class GameState {
+   private:
     /* data */
     std::string player1;
     std::string player2;
     LinkedList player1Hand;
     LinkedList player2Hand;
-public:
+
+   public:
     GameState();
     ~GameState();
 
-
     void load(std::string fileName);
-
 };
-
 
 #endif
--- Player.cpp
+++ Player.cpp
@@ -3,36 +3,26 @@
 #include <string>
 
 Player::Player(std::string playerName) {
-    playerHand = new Hand();
-    this->playerName = playerName;
-    playerScore = 0;
+  playerHand = new Hand();
+  this->playerName = playerName;
+  playerScore = 0;
 }
 
-Player::~Player() {
-    delete playerHand;
-}
+Player::~Player() { delete playerHand; }
 
-int Player::handSize() {
-    playerHand->handSize();
-}
+int Player::handSize() { return playerHand->handSize(); }
 bool Player::hasTile(Colour colour, Shape shape) {
-    return playerHand->containsTile(colour, shape);
+  return playerHand->containsTile(colour, shape);
 }
-void Player::addTileToHand(Tile* tile) {
-    playerHand->addTile(tile);
-}
+void Player::addTileToHand(Tile* tile) { playerHand->addTile(tile); }
 
-Tile* Player::playTile(Tile* tile) {
-    return playerHand->retrieveTile(tile);
+Tile* Player::retrieveTile(Tile* tile) {
+  return playerHand->retrieveTile(tile);
 }
-std::string Player::getPlayerName() {
-    return playerName;
-}
+std::string Player::getPlayerName() { return playerName; }
 
-std::string Player::getPlayerHand() {
-    return playerHand->handDetails();
-}
+std::string Player::getPlayerHand() { return playerHand->handDetails(); }
 
-int Player::getPlayerScore() {
-    return playerScore;
-}
+int Player::getPlayerScore() { return playerScore; }
+
+void Player::addScore(int scoreModifier) { playerScore += scoreModifier; }
