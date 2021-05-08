c6680d342e86a4b6e9bedcff87385df64e658902
Matt <matt@curiousit.com>
2021-05-08 16:35:21 +1000

    Current GameState implementation

--- GameState.cpp
+++ GameState.cpp
@@ -3,47 +3,38 @@
 #include "TileFactory.h"
 
 GameState::GameState(){};
-GameState::GameState(Player* player1, Player* player2, Board* board, LinkedList* tileBag)
-    : player1(player1), player2(player2), board(board), tileBag(tileBag) {}
+GameState::GameState(Player* player1, Player* player2, LinkedList* tileBag,
+                     Board* board)
+    : player1(player1), player2(player2), tileBag(tileBag), board(board) {}
 
 GameState::~GameState(){};
 
 void GameState::initiatePlayer1(LinkedList* hand, std::string name, int score) {
-    player1 = new Player(name);
-    player1->addScore(score);
-    for (int i = 0; i < hand->size(); ++i) {
-        player1->addTileToHand(hand->takeFront());
-    }
+  player1 = new Player(name);
+  player1->addScore(score);
+  for (int i = 0; i < hand->size(); ++i) {
+    player1->addTileToHand(hand->takeFront());
+  }
 }
 
 void GameState::initiatePlayer2(LinkedList* hand, std::string name, int score) {
-    player2 = new Player(name);
-    player2->addScore(score);
-    for (int i = 0; i < hand->size(); ++i) {
-        player1->addTileToHand(hand->takeFront());
-    }
+  player2 = new Player(name);
+  player2->addScore(score);
+  for (int i = 0; i < hand->size(); ++i) {
+    player1->addTileToHand(hand->takeFront());
+  }
 }
 
-void GameState::initiateBoard(std::string boardInfo) {
-}
+void GameState::initiateBoard(std::string boardInfo) {}
 
 void GameState::initiateTileBag(std::string tileBagInfo) {
-    TileFactory tileFactory;
-    tileBag = tileFactory.createTileBag(tileBagInfo);
+  TileFactory tileFactory;
+  tileBag = tileFactory.createTileBag(tileBagInfo);
 }
 
-Player* GameState::getPlayer1() {
-    return player1;
-}
-Player* GameState::getPlayer2() {
-    return player2;
-}
-Board* GameState::getBoard() {
-    return board;
-}
-LinkedList* GameState::getTileBag() {
-    return tileBag;
-}
+Player* GameState::getPlayer1() { return player1; }
+Player* GameState::getPlayer2() { return player2; }
+Board* GameState::getBoard() { return board; }
+LinkedList* GameState::getTileBag() { return tileBag; }
 
-void GameState::load(std::string fileName) {
-}
+void GameState::load(std::string fileName) {}
--- GameState.h
+++ GameState.h
@@ -8,28 +8,29 @@
 #include "Player.h"
 
 class GameState {
-   private:
-    /* data */
-    Player* player1;
-    Player* player2;
-    LinkedList* tileBag;
-    Board* board;
+ private:
+  /* data */
+  Player* player1;
+  Player* player2;
+  LinkedList* tileBag;
+  Board* board;
 
-   public:
-    GameState();
-    GameState(Player* player1, Player* player2, Board* board, LinkedList* tileBag);
-    ~GameState();
+ public:
+  GameState();
+  GameState(Player* player1, Player* player2, LinkedList* tileBag,
+            Board* board);
+  ~GameState();
 
-    Player* getPlayer1();
-    Player* getPlayer2();
-    Board* getBoard();
-    LinkedList* getTileBag();
+  Player* getPlayer1();
+  Player* getPlayer2();
+  Board* getBoard();
+  LinkedList* getTileBag();
 
-    void initiatePlayer1(LinkedList* hand, std::string name, int score);
-    void initiatePlayer2(LinkedList* hand, std::string name, int score);
-    void initiateBoard(std::string boardInfo);
-    void initiateTileBag(std::string tileBagInfo);
-    void load(std::string fileName);
+  void initiatePlayer1(LinkedList* hand, std::string name, int score);
+  void initiatePlayer2(LinkedList* hand, std::string name, int score);
+  void initiateBoard(std::string boardInfo);
+  void initiateTileBag(std::string tileBagInfo);
+  void load(std::string fileName);
 };
 
 #endif
